08c67d7f6a23ab467ad9ff626bcd49be4255cb18
Clayton Astrom <clayton.astrom@morningstar.com>
2020-03-27 21:52:03 -0500

    Testing a failed coverage

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -84,6 +84,10 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
+
+  private static notAFunction(message: string) {
+    return message;
+  }
 }
 </script>
 
